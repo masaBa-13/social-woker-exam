6b4fe5318dc64d1021aec21aae897bce2a59ca82
masaBa-13 <masaharu2000.01.04@gmail.com>
2026-01-30 21:18:13 +0900

    Update next config for github pages

--- social-work-quiz/next.config.ts
+++ social-work-quiz/next.config.ts
@@ -1,8 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  // リポジトリ名に合わせて設定（先頭に / を忘れずに）
+  basePath: "/social-woker-exam",
 };
 
 export default nextConfig;
